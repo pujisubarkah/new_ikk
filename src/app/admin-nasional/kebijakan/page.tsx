281d72c71a8adfb69481e5c5eacc72c998764b7f
Pujiatmo Subarkah <pujiatmo.subarkah@gmail.com>
2025-04-19 07:57:58 +0700

    first commit

---
+++ src/app/admin-nasional/kebijakan/page.tsx
@@ -0,0 +1,9 @@
+export default function KebijakanPage() {
+    return (
+        <div style={{ textAlign: 'center', padding: '2rem' }}>
+            <h1>Kebijakan</h1>
+            <p>Halaman ini sedang dalam pengembangan.</p>
+            <p>Silakan kembali lagi nanti.</p>
+        </div>
+    );
+}
