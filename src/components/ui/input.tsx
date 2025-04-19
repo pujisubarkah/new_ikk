e643e41a4d5e4a0c84d050ba5794af623b63824e
Pujiatmo Subarkah <pujiatmo.subarkah@gmail.com>
2025-04-19 07:41:11 +0700

    first commit

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,7 +1,9 @@
-import * as React from "react"
-import { cn } from "@/lib/utils"
+import * as React from "react";
+import { cn } from "@/lib/utils";
 
-const Input = React.forwardRef<HTMLInputElement, React.InputHTMLAttributes<HTMLInputElement>>(({ className, type, ...props }, ref) => {
+type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, type, ...props }, ref) => {
   return (
     <input
       type={type}
@@ -12,8 +14,5 @@ const Input = React.forwardRef<HTMLInputElement, React.InputHTMLAttributes<HTMLI
       ref={ref}
       {...props}
     />
-  )
-})
-Input.displayName = "Input"
-
-export { Input }
+  );
+});
