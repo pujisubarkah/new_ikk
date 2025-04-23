fbf50b737075c37d4ca14d93aeb8b367d7d0b67a
Rezky Yayang Yakhamid <60925883+rezkyyayang@users.noreply.github.com>
2025-04-23 17:55:42 +0700

    Update sidebar-admin.tsx

--- src/components/sidebar-admin.tsx
+++ src/components/sidebar-admin.tsx
@@ -107,7 +107,7 @@ const Sidebar: React.FC<SidebarProps> = ({ children }) => {
         </header>
 
         {/* Page Content */}
-        <main className="py-8 px-6 mt-20">
+        <main className="py-8 px-6 mt-20 max-w-[calc(100%-16rem)]">
           {children}
         </main>
       </div>
