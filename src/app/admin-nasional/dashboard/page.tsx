d589465a558838e6b37bb30e5c678b1ef7f8225a
Rezky Yayang Yakhamid <60925883+rezkyyayang@users.noreply.github.com>
2025-04-23 16:25:31 +0700

    Update page.tsx

--- src/app/admin-nasional/dashboard/page.tsx
+++ src/app/admin-nasional/dashboard/page.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
-import Header from '@/components/header';
 import Sidebar from '@/components/sidebar-admin';
 
 const DashboardPage: React.FC = () => {
   return (
     <div className="flex min-h-screen">
       <Sidebar>
-        {/* Add any child elements or leave it empty if no children are needed */}
-        <div></div>
-      </Sidebar>
+      
       <div className="flex-1">
         <Header />
         <main className="p-6">
@@ -16,6 +13,7 @@ const DashboardPage: React.FC = () => {
           {/* Konten dashboard lainnya bisa ditambahkan di sini */}
         </main>
       </div>
+        </Sidebar>
     </div>
   );
 };
