e9ea2d6b812e757aa4174ea19d58f020ecb25beb
pujisubarkah <67720528+pujisubarkah@users.noreply.github.com>
2025-04-24 22:42:31 +0700

    Add files via upload

---
+++ src/app/403/page.tsx
@@ -0,0 +1,8 @@
+export default function ForbiddenPage() {
+    return (
+      <div className="flex items-center justify-center min-h-screen bg-gray-100 text-red-600">
+        <h1 className="text-3xl font-bold">🚫 403 - Anda tidak punya akses ke halaman ini</h1>
+      </div>
+    )
+  }
+  
