5c702d54415db9a1984407628000d23bed93716b
pujisubarkah <67720528+pujisubarkah@users.noreply.github.com>
2025-04-24 20:47:06 +0700

    Update tabs.tsx

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -2,7 +2,6 @@
 
 import * as React from "react"
 import * as TabsPrimitive from "@radix-ui/react-tabs"
-import { cva } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const Tabs = TabsPrimitive.Root
