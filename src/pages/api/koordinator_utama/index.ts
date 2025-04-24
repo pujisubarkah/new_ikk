f9f595765056b5c26a476b25b5bc13d1ceba5845
pujisubarkah <67720528+pujisubarkah@users.noreply.github.com>
2025-04-24 22:34:52 +0700

    Add files via upload

---
+++ src/pages/api/koordinator_utama/index.ts
@@ -0,0 +1,33 @@
+"use client";
+
+import { NextApiRequest, NextApiResponse } from 'next'
+import prisma from '@/lib/prisma'
+import { serializeBigInt } from '@/lib/serializeBigInt'
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { id } = req.query
+
+  if (req.method === 'GET') {
+    if (!id) {
+      return res.status(400).json({ error: 'Parameter id is required' })
+    }
+
+    try {
+      const data = await prisma.koordinator_utama_koordinator_instansi.findMany({
+        where: {
+          koordinator_utama_id: BigInt(id as string),
+        },
+        include: {
+          user_koordinator_utama_koordinator_instansi_koordinator_instansi_idTouser: true,
+        },
+      })
+
+      res.status(200).json(serializeBigInt(data))
+    } catch (error) {
+      console.error('Error:', error)
+      res.status(500).json({ error: 'Internal Server Error' })
+    }
+  } else {
+    res.status(405).json({ error: 'Method Not Allowed' })
+  }
+}
