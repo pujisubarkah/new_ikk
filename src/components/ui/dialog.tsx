135ad04ed3f342cd449695a296e1beb129d69ad7
pujisubarkah <67720528+pujisubarkah@users.noreply.github.com>
2025-04-28 21:08:10 +0700

    Update dialog.tsx

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -1,5 +1,12 @@
 import * as React from "react"
-import { Dialog as RadixDialog, DialogTrigger, DialogContent, DialogTitle } from "@radix-ui/react-dialog"
+import {
+  Dialog as RadixDialog,
+  DialogPortal,
+  DialogOverlay,
+  DialogTrigger,
+  DialogContent as RadixDialogContent,
+  DialogTitle as RadixDialogTitle,
+} from "@radix-ui/react-dialog"
 
 export interface DialogProps {
   open: boolean
@@ -18,5 +25,28 @@ const Dialog = React.forwardRef<HTMLDivElement, DialogProps>((props) => {
 })
 
 Dialog.displayName = "Dialog"
+
+// Custom DialogContent with centering, overlay and scroll
+const DialogContent = React.forwardRef<
+  HTMLDivElement,
+  React.ComponentPropsWithoutRef<typeof RadixDialogContent>
+>(({ children, className = "", ...props }, ref) => (
+  <DialogPortal>
+    {/* Overlay */}
+    <DialogOverlay className="fixed inset-0 bg-black/50" />
+    {/* Content */}
+    <RadixDialogContent
+      ref={ref}
+      {...props}
+      className={`fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white p-6 rounded-lg shadow-lg w-full max-w-md max-h-[80vh] overflow-y-auto ${className}`}
+    >
+      {children}
+    </RadixDialogContent>
+  </DialogPortal>
+))
+
+DialogContent.displayName = "DialogContent"
+
+const DialogTitle = RadixDialogTitle
 
 export { Dialog, DialogTrigger, DialogContent, DialogTitle }
