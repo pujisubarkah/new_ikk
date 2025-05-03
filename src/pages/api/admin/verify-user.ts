a5688c5f7c6ff7eb981b793c40ab774cc5cb50fd
pujisubarkah <67720528+pujisubarkah@users.noreply.github.com>
2025-05-03 20:08:03 +0700

    Update verify-user.ts

--- src/pages/api/admin/verify-user.ts
+++ src/pages/api/admin/verify-user.ts
@@ -22,7 +22,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       });
 
       return res.status(200).json(updatedUser);
-    } catch (error) {
+    } catch {
       return res.status(500).json({ message: 'Failed to verify user' });
     }
   }
