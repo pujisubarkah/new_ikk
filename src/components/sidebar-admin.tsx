6b0652834d5916ebc94b97eab6dbf5f19bd462c1
pujisubarkah <67720528+pujisubarkah@users.noreply.github.com>
2025-05-04 20:35:40 +0700

    Update sidebar-admin.tsx

--- src/components/sidebar-admin.tsx
+++ src/components/sidebar-admin.tsx
@@ -52,6 +52,7 @@ const Sidebar: React.FC<SidebarProps> = ({ children }) => {
       items: [
         { name: 'Tahun Aktif', icon: Calendar, href: '/tahun-aktif' },
         { name: 'Instansi', icon: Users, href: '/instansi' },
+        { name: 'Kelompok Validator', icon: Users, href: '/group_validator' },
         { name: 'Pengguna', icon: Users, href: '/pengguna' },
       ],
     },
