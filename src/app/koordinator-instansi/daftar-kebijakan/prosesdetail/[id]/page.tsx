88114533a9584c6e8e981277963fdd83ab4d015f
pujisubarkah <67720528+pujisubarkah@users.noreply.github.com>
2025-05-07 15:55:47 +0700

    Add files via upload

---
+++ src/app/koordinator-instansi/daftar-kebijakan/prosesdetail/[id]/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ProsesDetailPage = () => {
+    return (
+        <div>
+            <h1>Halaman Proses Detail</h1>
+            <p>Ini adalah halaman contoh untuk menampilkan detail proses berdasarkan ID.</p>
+        </div>
+    );
+};
+
+export default ProsesDetailPage;
